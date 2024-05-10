d9923bea06daad105390d2cfc1b7f53f2ff96762
Brandon Chung <bchung831@gmail.com>
2024-05-09 21:46:57 -0700

    Welcome

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Brandon Chung',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  subHeading: 'Welcome ',
 
   // social
   socialLinks: [
